d153c82a87f9d0ca1ca7237076d1a3662f7aafe3
dilidili <466311467@qq.com>
2018-05-20 15:38:04 +0800

    buffer

---
+++ Buffer/bufferinit.js
@@ -0,0 +1,8 @@
+// buffer 再console中使用十六进制进行展示
+console.log(Buffer.alloc(10))
+console.log(Buffer.alloc(20))
+console.log(Buffer.alloc(6,1))
+console.log(Buffer.allocUnsafe(10))
+console.log(Buffer.from([1,2,4]));
+console.log(Buffer.from('test'));
+
