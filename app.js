9e16204f4fd023d0eb84afefbff283cd3f0a7f97
dilidili <466311467@qq.com>
2018-07-05 10:11:59 +0800

    update

--- app.js
+++ app.js
@@ -4,25 +4,32 @@ const swig = require('swig')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Cookies = require('cookies')
-//初始化express
+const CookieParser = require('cookie-parser')
+// 初始化express
 const app = express()
 
 // 设置静态文件托管
-//当用户访问的url以/public开始，那么直接返回对应__dirname + '/public'下的文件
+// 当用户访问的url以/public开始，那么直接返回对应__dirname + '/public'下的文件
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
-//配置应用模板
-//定义当前应用所使用的模板引擎
-//第一个参数：模板引擎的名称，同时也是模板文件的后缀，第二个参数表示用于解析处理模板内容的方法
+// 配置应用模板
+// 定义当前应用所使用的模板引擎
+// 第一个参数：模板引擎的名称，同时也是模板文件的后缀，第二个参数表示用于解析处理模板内容的方法
 app.engine('html', swig.renderFile)
-//设置模板文件存放的目录，第一个参数必须是views，第二个参数是目录
+// 设置模板文件存放的目录，第一个参数必须是views，第二个参数是目录
 app.set('views', './views')
-//注册所使用的模板引擎，第一个参数必须是 view engine，第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）是一致的
+// 注册所使用的模板引擎，第一个参数必须是 view engine，第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）是一致的
 app.set('view engine', 'html')
-//在开发过程中，需要取消模板缓存
+// 在开发过程中，需要取消模板缓存
 swig.setDefaults({cache: false})
 // 定义bpdyParsaer中间件设置
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(CookieParser())
+
+app.use((req, res, next) => {
+  req.cookies = new Cookies()
+  next()
+})
 
 // 根据不同的功能划分不同的路由
 app.use('/admin', require(path.join(__dirname, '/routers/admin')))
@@ -36,5 +43,3 @@ mongoose.connect('mongodb://localhost:27017/myblog', (err) => {
     console.log(`server running at port 3000 `)
   })
 })
-
-
--- routers/api.js
+++ routers/api.js
@@ -47,26 +47,71 @@ router.post('/user/register', (req, res, next) => {
   // 查询数据库中是否存在这个用户名
   User.findOne({
     username
-  }).then((userInfo) => {
-    if (userInfo) {
-      // 数据库中有该条记录
+  })
+    .then(userInfo => {
+      if (userInfo) {
+        // 数据库中有该条记录
+        responseData = {
+          code: 1,
+          message: '用户名已经被注册'
+        }
+        res.json(responseData)
+        return null
+      } else {
+        // 保存用户信息到数据库
+        const user = new User({
+          username,
+          password
+        })
+        return user.save()
+      }
+    })
+    .then(newUserInfo => {
+      if (newUserInfo) {
+        responseData.message = '注册成功'
+        res.json(responseData)
+      }
+    })
+})
+
+// 用户登录
+router.post('/user/login', (req, res, next) => {
+  const {username, password} = req.body
+  if (username === '' || password === '') {
+    responseData = {
+      code: 1,
+      message: '用户名或密码不能为空'
+    }
+    res.json(responseData)
+    return
+  }
+  // 查询数据库中相同用户名和密码的记录是否存在
+  User.findOne({
+    username,
+    password
+  }).then(userInfo => {
+    if (!userInfo) {
+      // 用户名或密码错误
       responseData = {
-        code: 1,
-        message: '用户名已经被注册'
+        code: 2,
+        message: '用户名或密码错误'
       }
       res.json(responseData)
-      return null
     } else {
-      // 保存用户信息到数据库
-      const user = new User({
-        username,
-        password
-      })
-      return user.save()
-    }
-  }).then((newUserInfo) => {
-    if (newUserInfo) {
-      responseData.message = '注册成功'
+      let {_id, username} = userInfo
+      // 登陆成功
+      // 返回登录信息给客户端
+      responseData.message = '登陆成功'
+      responseData.userInfo = {
+        _id,
+        username
+      }
+
+      // 对浏览器设置一个cookie保存登录状态
+      req.cookies.set('userInfo', JSON.stringify({
+        _id,
+        username
+      }))
       res.json(responseData)
     }
   })
