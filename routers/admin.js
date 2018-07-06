3240d17803e6d2f841b5dbd54ff63b72f635d366
dilidili <466311467@qq.com>
2018-07-06 10:41:37 +0800

    分类管理后端完成

--- routers/admin.js
+++ routers/admin.js
@@ -33,7 +33,7 @@ router.get('/user', (req, res) => {
    *
    **/
   let page = req.query.page || 1
-  let limit = 2
+  let limit = 30
   let pages = 0
   let skip = 0
   let countNum = 0
@@ -63,8 +63,32 @@ router.get('/user', (req, res) => {
 
 // 文章分类  分类管理
 router.get('/category', (req, res) => {
-  res.render('admin/category_index', {
-    userInfo: req.userInfo
+  let page = req.query.page || 1
+  let limit = 30
+  let pages = 0
+  let skip = 0
+  let countNum = 0
+
+  Category.count().then(count => {
+    // 计算总页数
+    pages = Math.ceil(count / limit)
+    // 取值不能超过pages
+    page = Math.min(page, pages)
+
+    skip = (page - 1) * limit > 0 ? (page - 1) * limit : 0
+    countNum = count
+    return Category.find().limit(limit).skip(skip)
+  }).then(categories => {
+    res.render('admin/category_index', {
+      userInfo: req.userInfo,
+      categories: categories,
+      count: countNum,
+      page,
+      limit,
+      pages,
+      skip
+    })
+
   })
 })
 
@@ -100,10 +124,92 @@ router.post('/category/add', (req, res) => {
       }).save()
     }
   }).then(newCategory => {
-    res.render('admin/success',{
-      userInfo:req.userInfo,
-      message:'保存成功',
-      url:'/admin/category'
+    res.render('admin/success', {
+      userInfo: req.userInfo,
+      message: '保存成功',
+      url: '/admin/category'
+    })
+  })
+})
+
+// 文章分类 分类修改
+router.get('/category/edit', (req, res) => {
+  // 获取要修改的分类的信息并用表单的形式展现
+  const _id = req.query.id || ''
+  // 获取要修改的分类信息
+  Category.findOne({_id}).then(category => {
+    if (!category) {
+      res.render('admin/error', {
+        userInfo: req.userInfo,
+        message: '分类名不存在'
+      })
+      return Promise.reject()
+    } else {
+      res.render('admin/category_edit', {
+        userInfo: req.userInfo,
+        category
+      })
+    }
+  })
+})
+
+router.post('/category/edit', (req, res) => {
+  const {name} = req.body
+  const _id = req.query.id || ''
+
+  // 判断分类名是否存在
+  Category.findOne({_id}).then(category => {
+    if (!category) {
+      res.render('admin/error', {
+        userInfo: req.userInfo,
+        message: '分类信息不存在'
+      })
+      return Promise.reject()
+    } else {
+      if (name === category.name) {
+        // 当用户没有做任何修改的时候  直接返回修改成功
+        res.render('admin/error', {
+          userInfo: req.userInfo,
+          message: '保存成功',
+          url: '/admin/category'
+        })
+        return Promise.reject()
+      } else {
+        // 要修改的分类名称是否再数据库种已存在  不允许修改
+        return Category.findOne({
+          _id: {$ne: _id},
+          name
+        })
+      }
+    }
+  }).then(sameCategory => {
+    if (sameCategory) {
+      res.render('admin/error', {
+        userInfo: req.userInfo,
+        message: '分类名称已存在相同分类'
+      })
+      return Promise.reject()
+    } else {
+      return Category.update({_id}, {name})
+    }
+  }).then(() => {
+    res.render('admin/error', {
+      userInfo: req.userInfo,
+      message: '修改成功',
+      url: '/admin/category'
+    })
+  })
+})
+// 文章分类 分类删除
+router.get('/category/delete', (req, res) => {
+  const _id = req.query.id || ''
+  Category.remove({
+    _id
+  }).then(() => {
+    res.render('admin/error', {
+      userInfo: req.userInfo,
+      message: '删除成功',
+      url: '/admin/category'
     })
   })
 })
